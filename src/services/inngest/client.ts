2e63b33b356c177eca26799031b8c863749cabfe
Carlos Mostek <carlos.mostek@okta.com>
2025-01-17 12:07:39 -0600

    Hello World version

---
+++ src/services/inngest/client.ts
@@ -0,0 +1,4 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const client = new Inngest({ id: "my-app" });
---
+++ src/services/inngest/functions.ts
@@ -0,0 +1,10 @@
+import { client } from "./client";
+
+export const helloWorld = client.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
