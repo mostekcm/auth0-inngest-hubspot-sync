8810fde13707ee5bafe9455b2578097ea90368a7
Carlos Mostek <carlosmostek@gmail.com>
2025-04-09 20:14:26 -0500

    add logging events

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 // src/app/api/inngest/route.ts
 import { serve } from "inngest/next";
 import { client } from "@/services/inngest/client";
-import { handleAuth0Event } from "@/services/inngest/functions"; // Your own functions
+// import { handleAuth0Event } from "@/services/inngest/functions"; // Your own functions
 import { handleLogEvent } from "@/services/inngest/logging";
 
 export const { GET, POST, PUT } = serve({
   client,
-  functions: [handleAuth0Event, handleLogEvent],
+  functions: [handleLogEvent],
 });
